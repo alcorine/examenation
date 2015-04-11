78c2d09bfc5f08f264e8395f461e82476edaab59
Alco <evgenii_gonchar@mail.ru>
2015-04-11 17:18:29 +0300

    exam

--- wp-content/themes/FirstWPTheme/sidebar.php
+++
@@ -1,7 +0,0 @@
-<aside class='sidebar'>
-    <?php if(!dynamic_sidebar('sidebar')): ?>
-    <article class='categories sidebar-block'>
-                <h2>Categories</h2>
-        </article>
-    <?php endif; ?>
-</aside>
